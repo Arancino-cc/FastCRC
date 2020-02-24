10b65d26ad6be0bdc57dc600a66f11869f3e2be9
Mimmo <mimmo@smartme.io>
2020-02-24 11:49:07 +0100

    Support for NRF52

--- FastCRC_tables.h
+++ FastCRC_tables.h
@@ -32,7 +32,7 @@
 #include "inttypes.h"
 
 #if !defined(__SAM3X8E__)
-#if defined(__AVR__ ) || defined(__IMXRT1052__) || defined(__IMXRT1062__) || defined(ARDUINO_ARCH_STM32F1)
+#if defined(__AVR__ ) || defined(__IMXRT1052__) || defined(__IMXRT1062__) || defined(ARDUINO_ARCH_STM32F1) || defined(ARDUINO_ARCH_NRF52)
 #include <avr/pgmspace.h>
 #else
 #include <pgmspace.h>	
